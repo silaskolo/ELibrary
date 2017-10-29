05be930e08eb6b494a0f33c3e526ffdcb85fd9a1
tolujimoh <Ps123456>
2017-10-29 15:19:30 +0100

    search page

--- app/src/main/java/me/silaskolo/elibrary/SearchActivity.java
+++ app/src/main/java/me/silaskolo/elibrary/SearchActivity.java
@@ -1,16 +1,145 @@
 package me.silaskolo.elibrary;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String TAG = "elibrary";
+    EditText editTextSearch;
+    Spinner spinnerGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        editTextSearch = (EditText) findViewById(R.id.editTextSearch);
+        spinnerGroup = (Spinner) findViewById(R.id.spinnerGroup);
+
+        ArrayAdapter<CharSequence> groupAdapter = ArrayAdapter.createFromResource(this, R.array.group_array, android.R.layout.simple_spinner_item); // Create an ArrayAdapter using the string array and a default spinner layout
+        groupAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Specify the layout to use when the list of choices appears
+        spinnerGroup.setAdapter(groupAdapter); // Apply the adapter to the spinner
+
+        findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if user pressed on button register
+                //here we will register the user to server
+                runSearch();
+            }
+        });
+    }
+
+    private void runSearch() {
+        final String searchText = editTextSearch.getText().toString().trim();
+        final String searchGroup = spinnerGroup.getSelectedItem().toString();
+
+        //first we will do the validations
+
+        if (TextUtils.isEmpty(searchText)) {
+            editTextSearch.setError("Enter a First Name");
+            editTextSearch.requestFocus();
+            return;
+        }
+
+
+        //if it passes all the validations
+
+        class RunSearch extends AsyncTask<Void, Void, String> {
+
+            private ProgressBar progressBar;
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                //creating request handler object
+                RequestHandler requestHandler = new RequestHandler();
+
+                //creating request parameters
+                HashMap<String, String> params = new HashMap<>();
+
+                String mUrl;
+
+                switch (searchGroup){
+                    case "author":
+                        mUrl = URLs.URL_AUTHOR_SEARCH;
+                        break;
+                    case "book":
+                    default:
+                        mUrl = URLs.URL_BOOK_SEARCH;
+                        break;
+                    case "category":
+                        mUrl = URLs.URL_CATEGORY_SEARCH;
+                        break;
+                }
+
+                try {
+                    mUrl += URLEncoder.encode(searchText, "UTF-8");
+                }catch (UnsupportedEncodingException e){
+                    e.printStackTrace();
+                }
+
+
+                //returing the response
+                return requestHandler.sendPostRequest(mUrl, params);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                //displaying the progress bar while user registers on the server
+                progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.d("myTag", s);
+                //hiding the progressbar after completion
+                progressBar.setVisibility(View.GONE);
+
+                try {
+                    //converting response to json object
+                    JSONObject obj = new JSONObject(s);
+
+                    //if no error in response
+                    if (!obj.getBoolean("error")) {
+                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                        Log.d(TAG,obj.toString());
+
+                        //starting the profile activity
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Some error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //executing the async task
+        RunSearch rs = new RunSearch();
+        rs.execute();
     }
 
     public void onClickOpenDashboardActivity(View v) {
--- app/src/main/java/me/silaskolo/elibrary/URLs.java
+++ app/src/main/java/me/silaskolo/elibrary/URLs.java
@@ -1,23 +1,27 @@
 package me.silaskolo.elibrary;
 
-/**
- * Created by tolujimoh on 26/10/2017.
- */
-
-
 public class URLs {
 
-    private static final String AUTH_URL = "http://silaskolo.me/elibrary/service/user.php?action=";
-    private static final String BOOK_URL = "http://silaskolo.me/elibrary/service/books.php?action=";
-    private static final String CATEGORY_URL = "http://silaskolo.me/elibrary/service/category.php?action=";
+    private static final String APP_ROOT = "http://silaskolo.me/elibrary/";
+    private static final String AUTH_URL = APP_ROOT + "service/user.php?action=";
+    private static final String BOOK_URL = APP_ROOT + "service/books.php?action=";
+    private static final String CATEGORY_URL = APP_ROOT + "service/category.php?action=";
+    private static final String AUTHOR_URL = APP_ROOT + "service/author.php?action=";
+
+    public static final String BOOK_PATH = APP_ROOT + "uploads/books/";
 
     public static final String URL_REGISTER = AUTH_URL + "register";
     public static final String URL_LOGIN= AUTH_URL + "login";
+
     public static final String URL_BOOK_RECOMMENDED = BOOK_URL + "recommended";
     public static final String URL_BOOK_NEW = BOOK_URL + "new";
     public static final String URL_BOOK_BY_ID = BOOK_URL + "preview&bookID=" ;
-    public static final String BOOK_PATH = "http://silaskolo.me/elibrary/uploads/books/";
+    public static final String URL_BOOK_SEARCH = BOOK_URL + "search&searchText=" ;
+
     public static final String URL_CATEGORY_ALL = CATEGORY_URL + "all";
     public static final String URL_CATEGORY_BOOKS = CATEGORY_URL + "books&categoryID=";
+    public static final String URL_CATEGORY_SEARCH = CATEGORY_URL + "search&searchText=";
+
+    public static final String URL_AUTHOR_SEARCH = AUTHOR_URL + "search&searchText=";
 
 }
